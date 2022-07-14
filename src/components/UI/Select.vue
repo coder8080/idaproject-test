cacd0733fae36c5fef2b7fb00813f1d8cfc83c44
Roman Kolerov <coder8080@mail.ru>
2022-07-14 20:32:17 +0300

    Adding select

---
+++ src/components/UI/Select.vue
@@ -0,0 +1,44 @@
+<template>
+  <select class="select text-small" @change="onChange">
+    <option
+      v-for="option of options"
+      :key="option.value"
+      :value="option.value"
+      :selected="option.value == modelValue"
+    >
+      {{ option.text }}
+    </option>
+  </select>
+</template>
+<script>
+export default {
+  name: 'AppSelect',
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    modelValue: String,
+  },
+  methods: {
+    onChange(event) {
+      console.log('change')
+      this.$emit('update:modelValue', event.target.value)
+    },
+  },
+}
+</script>
+<style scoped>
+.select {
+  background-color: var(--container-color);
+  box-shadow: var(--input-shadow);
+  border-radius: var(--border-radius);
+  padding: 12px 16px;
+  transition: box-shadow 0.15s, 0.15s;
+}
+
+.select:hover,
+.select:active {
+  box-shadow: var(--input-shadow-hover);
+}
+</style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,4 +1,5 @@
 import AppField from '@/components/UI/Field'
 import AppButton from '@/components/UI/Button'
+import AppSelect from '@/components/UI/Select'
 
-export default [AppField, AppButton]
+export default [AppField, AppButton, AppSelect]
